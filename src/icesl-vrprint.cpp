3dc1ffd12e8a3ee2706d5d911920dba2aeb95944
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2020-01-15 18:47:08 +0100

    bridge detection (does not work)

--- src/icesl-vrprint.cpp
+++ src/icesl-vrprint.cpp
@@ -75,7 +75,21 @@ float         g_AutoPauseOverlapLen  = 5.0f;
 
 bool          g_InDangling = false;
 double        g_InDanglingStart = 0.0;
+bool          g_InDanglingBridging = false;
 std::map<int, float> g_DanglingHisto;
+
+class TrajPoint {
+public:
+  v3d   pos;
+  float th;
+  float r;
+  float ov;
+  float dg;
+  TrajPoint(v3d _pos, float _th, float _r,  float _ov, float _dg) :
+    pos(_pos), th(_th), r(_r), ov(_ov), dg(_dg) {}
+};
+
+std::vector<TrajPoint> g_DanglingTrajectory;
 
 bool          g_InOverlap = false;
 double        g_InOverlapStart = 0.0;
@@ -85,6 +99,8 @@ bool          g_DumpHeightField = false;
 double        g_DumpHeightFieldStartLen = 0.0;
 
 bool          g_Paused = false;
+
+std::vector<v3d> g_Trajectory;
 
 std::string   g_GCode_string;
 
@@ -112,8 +128,6 @@ bool   g_Dragging = false;
 float  g_Zoom = 1.0f;
 float  g_ZoomTarget = 1.0f;
 
-std::vector<v3d>                 g_Trajectory;
-
 typedef struct
 {
   double deplength;
@@ -129,6 +143,8 @@ AAB<3>                   g_HeightFieldBox;
 Array2D<Tuple<float,1> > g_HeightField;
 
 v3d    g_PrevPos(0.0);
+v3d    g_PrevPrevPos(0.0);
+bool   g_PrevWasTravelOrDangling = false;
 
 bool   g_ForceRedraw = true;
 bool   g_ForceClear  = false;
@@ -189,6 +205,8 @@ void printer_reset()
 {
   gcode_reset();
   g_PrevPos      = v3d(0.0);
+  g_PrevPrevPos  = v3d(0.0);
+
   g_Trajectory    .clear();
   g_HeightSegments.clear();
   g_GlobalDepositionLength = 0.0f;
@@ -204,7 +222,9 @@ void printer_reset()
   g_HeightField.fill((float)prev_z);
   motion_reset(g_FilamentDiameter);
   // stats
+  g_DanglingTrajectory.clear();
   g_InDangling = false;
+  g_InDanglingBridging = false;
   g_DanglingHisto.clear();
   g_InOverlap = false;
   g_OverlapHisto.clear();
@@ -701,6 +721,8 @@ bool step_simulation(bool gpu_draw)
     float dangling = 0.0f;
     float overlap  = 0.0f;
 
+    TrajPoint tj   = TrajPoint(pos, (float)th, (float)0.0f, dangling, overlap);
+
     // std::cerr << gcode_line() << std::endl;
 
     if (len > 1e-6 && !motion_is_travel()) {
@@ -715,6 +737,8 @@ bool step_simulation(bool gpu_draw)
       double squash_t = min(th / 2.0, r);
       double rs = disk_squashed_radius(r, squash_t);
       double max_th = motion_get_current_e_per_xyz() * cs / g_NozzleDiameter;
+
+      tj = TrajPoint(pos, (float)th, (float)r, dangling, overlap);
 
       //if (rs > 0.3f) {
       //  std::cerr << sprint("z %.6f th %.6f th_prev %.6f rs %.6f \n", (float)pos[2], (float)th, (float)th_prev, (float)rs);
@@ -732,7 +756,7 @@ bool step_simulation(bool gpu_draw)
       if (pos[2] > g_StatsHeightThres) {
 
         dangling = danglingAt((float)max_th, v3f(pos), (float)rs);
-        overlap = overlapAt((float)th, v3f(pos), (float)rs - raster_erode);
+        overlap  = overlapAt((float)th, v3f(pos), (float)rs - raster_erode);
 
         // dangling only if > 60%
         dangling = max(dangling - 0.6f, 0.0f) / 0.4f;
@@ -743,6 +767,11 @@ bool step_simulation(bool gpu_draw)
 
       // add segment to global length
       g_GlobalDepositionLength += len;
+
+      // add pos to dangling section if potentially bridging
+      if (g_InDangling && g_InDanglingBridging) {
+        g_DanglingTrajectory.push_back(tj);
+      }
 
       if (gpu_draw) {
         g_Bead.addPoint(v3f(pos), (float)th, (float)r, dangling, overlap);
@@ -762,10 +791,11 @@ bool step_simulation(bool gpu_draw)
       }
     }
 
-    g_PrevPos = pos;
+    bool is_travel_or_dangling = motion_is_travel() || (dangling > 0.0);
 
     // stats
     if (g_InDangling && dangling == 0.0f) {
+      // exit dangling
       g_InDangling = false;
       double dangling_len = (g_GlobalDepositionLength - g_InDanglingStart);
       if (g_AutoPause && dangling_len >= g_AutoPauseDanglingLen) {
@@ -773,9 +803,32 @@ bool step_simulation(bool gpu_draw)
       }
       dangling_len = round(dangling_len / 0.1); // quantize
       g_DanglingHisto[(int)dangling_len] += 1.0f;
-    
+      // bridge?
+      bool is_bridge = false;
+      if (!is_travel_or_dangling && g_InDanglingBridging  // attached on both ends
+        && g_DanglingTrajectory.size() >= 2) {
+        is_bridge = true;
+        // verify deviation
+        v2d delta = normalize_safe(v2d(g_DanglingTrajectory.back().pos - g_DanglingTrajectory.front().pos));
+        v2d nrm   = v2d(-delta[1], delta[0]);
+        for (auto p : g_DanglingTrajectory) {
+          float dev = dot(normalize_safe(v2d(p.pos - g_DanglingTrajectory.front().pos)), nrm);
+          if (abs(dev) > g_NozzleDiameter / 2.0f) {
+            is_bridge = false; break;
+          }
+        }
+        if (is_bridge) {
+          g_Paused = true;
+          g_Trajectory.clear();
+          for (auto p : g_DanglingTrajectory) {
+            g_Trajectory.push_back(p.pos);
+          }
+        }
+      }
+      g_DanglingTrajectory.clear();
     }
     if (g_InOverlap && overlap == 0.0f) {
+      // exit overlap
       g_InOverlap = false;
       double overlap_len = (g_GlobalDepositionLength - g_InOverlapStart);
       if (g_AutoPause && overlap_len >= g_AutoPauseOverlapLen) {
@@ -785,11 +838,17 @@ bool step_simulation(bool gpu_draw)
       g_OverlapHisto[(int)overlap_len] += 1.0f;
     }
     if (!g_InDangling && dangling > 0.0) {
-      g_InDangling = true;
-      g_InDanglingStart = g_GlobalDepositionLength;
+      // enter dangling
+      g_DanglingTrajectory.clear();
+      sl_assert(tj.r > 0.0f);
+      g_DanglingTrajectory.push_back(tj);
+      g_InDangling          = true;
+      g_InDanglingStart     = g_GlobalDepositionLength;
+      g_InDanglingBridging  = !g_PrevWasTravelOrDangling;
     }
     if (!g_InOverlap && overlap > 0.0) {
-      g_InOverlap = true;
+      // enter overlap
+      g_InOverlap      = true;
       g_InOverlapStart = g_GlobalDepositionLength;
     }
 
@@ -808,6 +867,11 @@ bool step_simulation(bool gpu_draw)
       }
     }
 #endif
+
+    // prepare next
+    g_PrevWasTravelOrDangling = is_travel_or_dangling;
+    g_PrevPrevPos = g_PrevPos;
+    g_PrevPos = pos;
 
   } // iter
   return false;
@@ -932,7 +996,8 @@ void mainRender()
     g_ShaderFinal.end();
 
     // render trajectory
-    if (g_ShowTrajectory && !g_Trajectory.empty()) {
+    // if (g_ShowTrajectory && !g_Trajectory.empty()) {
+    if (!g_Trajectory.empty()) {
       glEnable(GL_BLEND);
       glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
       glDisable(GL_DEPTH_TEST);
@@ -1162,7 +1227,7 @@ int main(int argc, const char **argv)
   LibSL::GPUHelpers::clearScreen(LIBSL_COLOR_BUFFER | LIBSL_DEPTH_BUFFER, 0.0f, 0.0f, 0.0f);
   g_RT->unbind();
 
-  g_GPUMesh_sphere = AutoPtr<MeshRenderer<mvf_mesh> >( new MeshRenderer<mvf_mesh>(shape_sphere( 1.0f, 12 )) );
+  g_GPUMesh_sphere   = AutoPtr<MeshRenderer<mvf_mesh> >( new MeshRenderer<mvf_mesh>(shape_sphere( 1.0f, 12 )) );
   g_GPUMesh_cylinder = AutoPtr<MeshRenderer<mvf_mesh> >(new MeshRenderer<mvf_mesh>(shape_cylinder(1.0f, 1.0f, 1.0f, 12)));
 
   /// default view
@@ -1234,6 +1299,7 @@ int main(int argc, const char **argv)
   }
 
 #endif
+
   motion_start( g_FilamentDiameter );
 
   printer_reset();
