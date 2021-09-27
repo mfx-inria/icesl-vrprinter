df74ac21c4daa93144f60c79c1a989c6d0810b18
Pierre Bedell <pierre.bedell@loria.fr>
2021-09-27 16:02:51 +0200

    AGPL licensing

--- src/icesl-vrprinter.h
+++ src/icesl-vrprinter.h
@@ -1,3 +1,23 @@
+/**
+  * IceSL-vrprinter, a tool to help simulate and visualize Gcode for 3D printers
+  * Copyright (C) 2021  Sylvain Lefebvre    sylvain.lefebvre@inria.fr
+  *                     Pierre Bedell       pierre.bedell@gmail.com
+  *                     Salim Perchy        salim.perchy@gmail.com
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Affero General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU Affero General Public License for more details.
+  *
+  * You should have received a copy of the GNU Affero General Public License
+  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
+
 #pragma once
 
 #include <LibSL.h>
--- src/shapes.cpp
+++ src/shapes.cpp
@@ -1,7 +1,26 @@
+/**
+  * IceSL-vrprinter, a tool to help simulate and visualize Gcode for 3D printers
+  * Copyright (C) 2021  Sylvain Lefebvre    sylvain.lefebvre@inria.fr
+  *                     Pierre Bedell       pierre.bedell@gmail.com
+  *                     Salim Perchy        salim.perchy@gmail.com
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Affero General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU Affero General Public License for more details.
+  *
+  * You should have received a copy of the GNU Affero General Public License
+  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
+
 // ------------------------------------------------------
 // Sylvain Lefebvre - 2013-01-29
 //-------------------------------------------------------
-
 #include <LibSL/LibSL.h>
 #include "shapes.h"
 
